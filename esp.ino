4067928a6771b884b1afc63ca847fd776745aee1
Adithya SM <63196135+Adithya65@users.noreply.github.com>
2022-05-30 20:52:06 +0530

    data updated

--- esp.ino
+++ esp.ino
@@ -1,6 +1,17 @@
- #include <ESP8266WiFi.h>
+   #include <ESP8266WiFi.h>
 #include <WiFiClientSecure.h>
+#define PIN A0
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_PCD8544.h>
+#include "DHT.h"
+#define DHTPIN D5
+DHT dht(DHTPIN, DHT11);
+#define rainAnalog A0
+ 
 
+Adafruit_PCD8544 display = Adafruit_PCD8544(D4,D3,D2,D1,D0);
+float resolution  = 3.3 / 1024;
 
 
 
@@ -12,8 +23,8 @@ int bn=0;
 int current=0;
 int current_iot=0;
  
-const char* ssid = "jio upstair"; 
-const char* password = "Adithya1"; 
+const char* ssid = "adi"; 
+const char* password = "qwerty12345"; 
 int x=0;
 const char* host = "script.google.com";
 const int httpsPort = 443;
@@ -21,13 +32,22 @@ const int httpsPort = 443;
 
 WiFiClientSecure client; 
 
-String GAS_ID = "AKfycbx9QnO3qUJditCvlrsozgO9CzuTwCZXWj726h0INjQI--xfbQbN"; 
+String GAS_ID = "AKfycbwUsBtULqAFRyixtySTfD6dS6MV_oXtEkR--GiUsnA4T6XDEB3HHBFMa8_ooetM17hF"; 
 
 
 void setup() {
-  pinMode(12,OUTPUT);//D6
-pinMode(14,INPUT);//D5
-pinMode(16,OUTPUT);
+  
+  pinMode(D6,OUTPUT);
+  pinMode(PIN, INPUT);
+  //DISPLAY
+   
+   display.begin();
+ 
+  //DHT
+  Serial.println(F("DHTxx test!"));
+  dht.begin();
+    
+ 
   Serial.begin(115200);
   delay(500);
 
@@ -39,7 +59,7 @@ pinMode(16,OUTPUT);
     
   pinMode(ON_Board_LED,OUTPUT); 
   digitalWrite(ON_Board_LED, HIGH); 
-  
+  digitalWrite(D6,HIGH);
   Serial.print("Connecting");
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
@@ -64,12 +84,50 @@ pinMode(16,OUTPUT);
 }
 
 void loop() {
-  
-  sendData( ); 
+     
+int curr=analogRead(PIN);                // Function to read from pin A0
+  Serial.print("Voltage (when zero current) = ");
+ 
+  delay(1000);
+  //Humidity
+
+  float humidity = dht.readHumidity();
+  float temp = dht.readTemperature();
+ 
+  //rain
+    int rain = analogRead(rainAnalog);
+
+
+ display.clearDisplay(); //clears display each time the loop starts over
+
+display.setCursor(0,0);
+
+display.print("The current is: "); //this will be written on the LCD
+
+display.println(curr);
+
+display.display();
+
+delay(1000);
+
+
+   sendData(temp,humidity,26,curr,rain );
 }
 
-void sendData( ) {
+int sendData(int temp,int humidity,int light,int curr,int rain ) {
 
+
+  display.clearDisplay(); //clears display each time the loop starts over
+
+display.setCursor(0,0);
+
+display.print("The current is: "); //this will be written on the LCD
+
+display.println(curr);
+
+display.display();
+
+delay(1000);
   current=current+10;
   Serial.println("==========");
   Serial.print("connecting to ");
@@ -77,7 +135,7 @@ void sendData( ) {
   
   if (!client.connect(host, httpsPort)) {
     Serial.println("connection failed");
-    return;
+    return 0;
   }
 
   if (bn==5){
@@ -89,7 +147,7 @@ void sendData( ) {
   current_iot=current;
   
   
-  String url = "/macros/s/" + GAS_ID + "/exec?temperature=" + String(current_iot) + "&humidity=" + String(current);
+  String url = "/macros/s/" + GAS_ID + "/exec?temperature=" + String(temp) + "&humidity="+String(humidity)+"&light="+String(light)+"&current="+String(curr)+"&preci="+String(rain)+"&units="+String(220*curr/100)+"&current_iot="+String(220*curr/100);
   Serial.print("requesting URL: ");
   Serial.println(url);
   if (k==Delay){
